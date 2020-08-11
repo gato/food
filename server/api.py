9058c49cc3c44ce1f04ae076e2bd4a6fe705798a
Marcelo Glezer <marcelo.glezer@gmail.com>
2020-08-11 09:47:18 -0300

    build and deploy with docker-composer

--- server/api.py
+++ server/api.py
@@ -8,4 +8,4 @@ api.add_resource(Food, '/food', '/food/<string:food_id>')
 api.add_resource(Foods, '/foods', '/foods') 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, host= '0.0.0.0')
